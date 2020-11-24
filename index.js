8d0a6fdd4746aaf9b02224b23bc2c143a95ba40f
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-24 01:42:34 -0500

    commit tree 4197e9ed42670d5d0d586ad8c9251d120086e0a6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(list,driver){
+    return list.filter(dr => {
+        return dr.toLowerCase() === driver.toLowerCase()
+    })
+}
+
+function fuzzyMatch(list,letter){
+    return list.filter(dr => {
+        return dr.charAt(0).toLowerCase() === letter.charAt(0).toLowerCase()
+    })
+}
+
+function matchName(list,name){
+    return list.filter(dr => {
+        return dr.name === name
+    })
+}
